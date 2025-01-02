6ed7d268cffd5f71446edeaf45cf42b8d75cbfcd
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2025-01-02 16:24:39 +0100

    Updates domain DNS entry in AWS Router53 and PowerDNS

--- Readme.md
+++ Readme.md
@@ -1,16 +1,18 @@
 # Home IP Updater
 
-This program is suscribed to [home-ip-monitor](https://git.windmaker.net/a-castellano/home-ip-monitor) update queue, it will update required DNS record with reases IP's from queue.
+[![pipeline status](https://git.windmaker.net/a-castellano/home-ip-updater/badges/master/pipeline.svg)](https://git.windmaker.net/a-castellano/home-ip-updater/pipelines)[![coverage report](https://git.windmaker.net/a-castellano/home-ip-updater/badges/master/coverage.svg)](https://a-castellano.gitpages.windmaker.net/home-ip-updater/coverage.html)[![Quality Gate Status](https://sonarqube.windmaker.net/api/project_badges/measure?project=a-castellano_home-ip-updater_533a7009-26fb-43b9-b6f3-eb5326c083b6&metric=alert_status&token=sqb_df6b40224599cede55c63c9203eb5fcdb0a4bc9e)](https://sonarqube.windmaker.net/dashboard?id=a-castellano_home-ip-updater_533a7009-26fb-43b9-b6f3-eb5326c083b6)
 
-# What this progam does?
+This program is subscribed to [home-ip-monitor](https://git.windmaker.net/a-castellano/home-ip-monitor) update queue, it will update required DNS record with readed IP's from queue.
 
-Reads IP's from configured queue and updated reqired DNS record.
+# What this utility does?
+
+Reads IP's from configured queue and updates required DNS record in AWS Route53 service and my local DNS server.
 
 # Required variables
 
 ## Queue names
 
-**UPDATE_QUEUE_NAME**: Queue name where new IP's will be sended.
+**UPDATE_QUEUE_NAME**: Queue name where new IP's will be sent.
 
 ## AWS Config
 
@@ -20,9 +22,9 @@ DNS service is hosted in AWS Route53 service, AWS auth and Route53 keys are requ
 **AWS_SECRET_ACCESS_KEY**: AWS account secret key
 **AWS_REGION**: AWS region, default value is "us-west-2"
 **AWS_ZONE_ID**: Route53 zone ID
-**SUBDOMAIN**: Subdomain record to update
 
 In this case, an user with limited permissions is created with the following policy:
+
 ```
 "Version": "2012-10-17",
     "Statement": [
@@ -38,6 +40,20 @@ In this case, an user with limited permissions is created with the following pol
     ]
 }
 ```
+
+## PowerDNS config
+
+This program uses PowerDSN API for updating records, the following env vars must be set:
+
+**POWER_DNS_API_HOST**: PowerDNS API IP
+**POWER_DNS_API_PORT**: PowerDNS API Port
+**POWER_DNS_API_KEY**: API key used to access PowerDNS API
+**POWER_DNS_ZONE_NAME**: DNS Zone name to update
+
+## Domain config
+
+Required domain A record will be updated, domain value is given by the following env variable:
+**SUBDOMAIN**: Subdomain record to update
 
 ## RabbitMQ Config
 
--- config/config.go
+++ config/config.go
@@ -4,16 +4,21 @@ import (
 	"cmp"
 	"errors"
 	"os"
+	"strconv"
 
 	rabbitmqconfig "github.com/a-castellano/go-types/rabbitmq"
 )
 
 // Config struct contians required config variables
 type Config struct {
-	AWSZoneID      string // home-ip-monitor will send new IP values to be updated if associated ISP is the same than this value
-	Subdomain      string // This will be the queue used to send IP changes
-	UpdateQueue    string // This will be the queue used to send IP changes
-	RabbitmqConfig *rabbitmqconfig.Config
+	AWSZoneID        string // home-ip-monitor will send new IP values to be updated if associated ISP is the same than this value
+	Subdomain        string // Subdomain to update
+	PowerDNSAPIKey   string // PowerDNS API key
+	PowerDNSHost     string // PowerDNS API Host
+	PowerDNSPort     int    // PowerDNS API Port
+	PowerDNSZoneName string // PowerDNS API Port
+	UpdateQueue      string // This will be the queue used to send IP changes
+	RabbitmqConfig   *rabbitmqconfig.Config
 }
 
 // NewConfig checks if required env variables are present, returns config instance
@@ -21,6 +26,7 @@ func NewConfig() (*Config, error) {
 	config := Config{}
 
 	var envVariableFound bool
+	var PowerDNSPortString string
 	// First check for AWS_ACCESS_KEY_ID env variable
 	if _, envVariableFound = os.LookupEnv("AWS_ACCESS_KEY_ID"); !envVariableFound {
 		return nil, errors.New("AWS_ACCESS_KEY_ID env variable must be set")
@@ -29,6 +35,21 @@ func NewConfig() (*Config, error) {
 	// Now check for AWS_SECRET_ACCESS_KEY
 	if _, envVariableFound = os.LookupEnv("AWS_SECRET_ACCESS_KEY"); !envVariableFound {
 		return nil, errors.New("AWS_SECRET_ACCESS_KEY env variable must be set")
+	}
+
+	// All PowerDNS variables are required
+	if config.PowerDNSHost, envVariableFound = os.LookupEnv("POWER_DNS_API_HOST"); !envVariableFound {
+		return nil, errors.New("POWER_DNS_API_HOST env variable must be set")
+	}
+	if PowerDNSPortString, envVariableFound = os.LookupEnv("POWER_DNS_API_PORT"); !envVariableFound {
+		return nil, errors.New("POWER_DNS_API_PORT env variable must be set")
+	}
+	config.PowerDNSPort, _ = strconv.Atoi(PowerDNSPortString)
+	if config.PowerDNSAPIKey, envVariableFound = os.LookupEnv("POWER_DNS_API_KEY"); !envVariableFound {
+		return nil, errors.New("POWER_DNS_API_KEY env variable must be set")
+	}
+	if config.PowerDNSZoneName, envVariableFound = os.LookupEnv("POWER_DNS_ZONE_NAME"); !envVariableFound {
+		return nil, errors.New("POWER_DNS_ZONE_NAME env variable must be set")
 	}
 
 	// Above env variables are not required in config
